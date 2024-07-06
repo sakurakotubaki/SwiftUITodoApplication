f3b3f03212d2a78e7b7ec139ab248af1d3b4bb73
Jboy422 <gitonly543@gmail.com>
2024-07-06 11:58:16 +0900

    Initial Commit

---
+++ SwiftUITodoApp/SwiftUITodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITodoAppApp.swift
+//  SwiftUITodoApp
+//
+//  Created by Jboy422 on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
